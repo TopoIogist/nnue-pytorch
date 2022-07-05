77abb602e96da53da8687b9c6ccd15cdda6e1bf9
Anon <name@email.com>
2022-07-05 21:40:04 +0200

    test

--- model.py
+++ model.py
@@ -327,7 +327,7 @@ class NNUE(pl.LightningModule):
     misclassified_draw = ((qf >= (0.5+misclassified_threshold)) | (qf <= (0.5-misclassified_threshold))) & draw_games
     misclassified = misclassified_won | misclassified_loss | misclassified_draw
 
-    loss = torch.pow(torch.abs(pt - qf)+4*min(torch.abs(pt - qf),torch.abs(qf - t))*(misclassified), 1.5).mean()
+    loss = torch.pow(torch.abs(pt - qf)+4*torch.min(torch.abs(pt - qf),torch.abs(qf - t))*(misclassified), 1.5).mean()
 
     self.log(loss_type, loss)
 
@@ -358,6 +358,6 @@ class NNUE(pl.LightningModule):
     # Increasing the eps leads to less saturated nets with a few dead neurons.
     # Gradient localisation appears slightly harmful.
  #   optimizer = ranger.Ranger(train_params, betas=(.9, 0.999), eps=1.0e-7, gc_loc=False, use_gc=False)
-    optimizer = Lamb(train_params,  lr= 5e-4, betas=(0.9, 0.999), eps=1e-7, weight_decay=0)
+    optimizer = Lamb(train_params,  lr= self.lr, betas=(0.9, 0.999), eps=1e-7, weight_decay=0)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=self.gamma)
     return [optimizer], [scheduler]
